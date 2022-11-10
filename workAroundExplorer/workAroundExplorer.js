c56a69e1d165e070407ceb9e82402d3a9c6b05ac
Sypher <85158811+Sypher12233@users.noreply.github.com>
2022-11-10 21:05:13 +0430

    adding new project workAroundExplorer

---
+++ workAroundExplorer/workAroundExplorer.js
@@ -0,0 +1,12 @@
+// WorkAround Explorer
+// WorkAround is a research organization that provides data on salary trends in the tech industry. Open up salaryData.js to see the data they have collected. You’ll notice that they have also created a few functions for filtering that data to get subsets of data by role and by company.
+
+// WorkAround now wants to release a new web application called WorkAround Explorer to make their data more easily viewable. This web app should allow users to choose specific roles and companies in the tech industry to see the following information:
+
+// The salary for the chosen role at the chosen company.
+// The industry average for the chosen role.
+// The average salary at the chosen company across all roles.
+// The industry average salary across all roles and all companies.
+// After the user selects a role from the first column and a company from the second, the relevant data is shown in the third column
+
+// Much of the user interface has already been designed by front-end developers, however, the core data and functionality are either missing or need to be connected together using modules. Your task is to help your team members out by using your import and export expertise!
