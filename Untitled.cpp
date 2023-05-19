cb0e22ca67486e177257aeaa7097bf678aa7de6b
Sypher <85158811+Sypher12233@users.noreply.github.com>
2023-05-19 11:27:40 +0430

    react notes to readme file of full Stack JS dir

---
+++ Untitled.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{cout<<"enter any two numbers to add";
+ int x;
+ int y;
+ cin>>x;
+ cin>>y;
+ int s=x+y;
+ cout<<"sum of " <<x<< "and "<<y<< "="<<s<< "\n";
+}
